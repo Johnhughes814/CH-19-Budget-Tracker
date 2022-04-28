64478499d511f72c7ddd16500f7053eedd234d7f
Johnhughes814 <8hughes.john@gmail.com>
2022-04-28 10:40:53 -0500

    more db.js

--- db.js
+++ db.js
@@ -18,3 +18,35 @@ request.onupgradeneeded = event => {
     }
 };
 
+request.onerror = event => console.error(event);
+
+function checkDatabase() {
+    const db = request.result;
+    let transaction = db.transaction([pendingObjectStoreName], `readwrite`);
+    let store = transaction.objectStore(pendingObjectStoreName);
+    const getAll = store.getAll();
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch(`/api/transaction/bulk`, {
+                method: `POST`,
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: `application/json, text/plain, */*`,
+                    "Content-Type": `application/json`
+                }
+            })
+                .then(response => response.json())
+                .then(() => {
+
+                    transaction = db.transaction([pendingObjectStoreName], `readwrite`);
+
+
+                    store = transaction.objectStore(pendingObjectStoreName);
+
+
+                    store.clear();
+                });
+        }
+    };
+}
